ed327a1d7695ffb4c203288f4531f8a82ceb3ef0
benjaminwills01 <you@example.com>
2024-09-24 19:35:45 +0200

    changes

--- week01/scripts/getDates.js
+++ week01/scripts/getDates.js
@@ -5,3 +5,24 @@ const lastModified = document.lastModified;
 document.getElementById(
   "lastModified"
 ).textContent = `Last Modified: ${lastModified}`;
+
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  menu.classList.toggle("active");
+  hamburger.textContent = hamburger.textContent === "✖" ? "☰" : "✖";
+});
+
+const darkModeToggle = document.getElementById("darkModeToggle");
+const mainElement = document.querySelector("main");
+
+darkModeToggle.addEventListener("click", () => {
+  mainElement.classList.toggle("dark-mode");
+
+  if (darkModeToggle.textContent === "🌙 Dark Mode") {
+    darkModeToggle.textContent = "☀️ Light Mode";
+  } else {
+    darkModeToggle.textContent = "🌙 Dark Mode";
+  }
+});
