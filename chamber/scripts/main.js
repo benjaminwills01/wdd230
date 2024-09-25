048fc0fe7727d1c788ec42b07d3c81d43d5026de
benjaminwills01 <you@example.com>
2024-09-25 21:57:18 +0200

    changes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,1 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.querySelector("nav ul");
+
+  hamburger.addEventListener("click", function () {
+    navMenu.classList.toggle("show");
+  });
+});
+
 document.getElementById("lastModified").textContent = document.lastModified;
