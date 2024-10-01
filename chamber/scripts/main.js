e13a8e7edaa846e8f2bcd5a6222196606a391243
benjaminwills01 <you@example.com>
2024-10-01 17:00:25 +0200

    changes

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -9,3 +9,29 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+/* Discover */
+
+document.addEventListener("DOMContentLoaded", () => {
+  const visitMessage = document.getElementById("visit-message");
+  const lastVisit = localStorage.getItem("lastVisit");
+  const currentVisit = new Date().getTime();
+
+  if (!lastVisit) {
+    visitMessage.textContent =
+      "Welcome! Let us know if you have any questions.";
+  } else {
+    const timeDiff = currentVisit - lastVisit;
+    const daysDiff = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+
+    if (daysDiff < 1) {
+      visitMessage.textContent = "Back so soon! Awesome!";
+    } else if (daysDiff === 1) {
+      visitMessage.textContent = "You last visited 1 day ago.";
+    } else {
+      visitMessage.textContent = `You last visited ${daysDiff} days ago.`;
+    }
+  }
+
+  localStorage.setItem("lastVisit", currentVisit);
+});
